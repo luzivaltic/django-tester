67489106990689a0282ed1cd8d957a5806086234
unknown <gokuhdvn3@gmail.com>
2022-07-28 17:29:07 +0700

    first commit

---
+++ firstapp/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render( request , "firstapp/index.html" )
+
+def binh(request):
+    return HttpResponse("Hello Binh")
+
+def greet(request , name):
+    return render(request, "firstapp/greet.html", {
+      "name": name.capitalize(),  
+    })
